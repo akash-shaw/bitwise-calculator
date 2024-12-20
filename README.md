88d8e6c6795ba20b45a9be2970f43829bffdf4b5
Akash Shaw <136084773+akash-shaw@users.noreply.github.com>
2024-12-20 16:39:35 +0530

    ADD: Fully functional web app

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# bitwise-calculator
-JavaScript Bitwise Calculator
+# Web Bitwise Calculator
+JavaScript Bitwise Calculator. View live [here](https://akash-shaw.github.io/bitwise-calculator/).
---
+++ script.js
@@ -0,0 +1,28 @@
+document.querySelector("#calculate").addEventListener('click', calculate);
+
+function calculate() {
+    const num1 = parseInt(document.querySelector('#number1').value);
+    const num2 = parseInt(document.querySelector('#number2').value);
+    const operation = document.querySelector('#operation').value;
+
+    if (isNaN(num1) || isNaN(num2)) {
+        document.querySelector('#result').textContent = "Invalid Input";
+        return;
+    }
+
+    let result;
+    switch (operation) {
+        case 'AND':
+            result = num1 & num2;
+            break;
+        case 'OR':
+            result = num1 | num2;
+            break;
+        case 'XOR':
+            result = num1 ^ num2;
+            break;
+    }
+
+    document.querySelector('#result').textContent = result;
+    document.querySelector('#binaryResult').textContent = `Binary: ${result.toString()}`;
+}
